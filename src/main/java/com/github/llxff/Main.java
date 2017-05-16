eb14e4e2c78df59ddb365ec99e2f3fdbf971e823
Александр Фомин <ll.wg.bin@gmail.com>
2017-05-16 22:31:29 +0300

    interface

--- src/main/java/com/github/llxff/Main.java
+++ src/main/java/com/github/llxff/Main.java
@@ -6,42 +6,87 @@ public class Main {
   }
 }
 
+// Класс для сохранения и загрузки состояния процессов и памяти
+class MachineStorage {
+  // Название файла, в котором хранится состояние
+  private String filename;
+
+  // Загрузка состояния
+  public VirtualMachine load() {}
+  // Сохранение состояния
+  public void save(VirtualMachine machine) {}
+}
+
+// Класс для управления процессами
 class VirtualMachine {
+  // Список процессов
   private LinkedList<Process> processes;
 
-  public VirtualMachine() {
-    VirtualMachine(new LinkedList<Process>());
-  }
+  // Объект для управления памятью
+  private Memory memory;
 
-  public VirtualMachine(LinkedList<Process> processes) {
-    this.processes = processes;
-  }
-
-  public Process add() {
-
-  }
+  // Создание нового процесса
+  public Process add() {}
+  // Завершение процесса
+  public void kill(int pid) {}
 }
 
-class Memory {
-  private int capacity;
-  private ArrayList<MemoryPage> pages;
-
-  public MemoryPage newPage() {
-
-  }
-}
-
-class Process {
+// Класс процесса
+class Process implements Serializable {
+  // Идентификатор процесса
   private int id;
+
+  // Массив используемыех страниц памяти
   private ArrayList<MemoryPage> pages;
+
+  // Объект для управления памятью
+  private Memory memory;
+
+  // Конструктор
+  public Process(int id, Memory memory) {}
+
+  // Добавление новой страницы памяти процессу
+  public void addMoreMemory(int amount) {}
+
+  // Завершить процесс и высвободить память
+  public void die() {}
 }
 
-class MemoryPage {
-  private int number;
-  private String status;
+// Класс для управления памятью
+class Memory implements Serializable {
+  // Объем памяти
   private int size;
 
-  public MemoryPage() {
+  // Выделенные страницы
+  private ArrayList<MemoryPage> pages;
 
-  }
+  // Конструктор
+  public Memory(int size) {}
+
+  // Выделить новую страницу памяти
+  public MemoryPage allocate() {}
+
+  // Высвободить указанную страницу
+  public void free(MemoryPage page) {}
+
+  // Использованный объем памяти
+  public int usedAmount() {}
+
+  // Свободный объем памяти
+  public int freeAmount() {}
+}
+
+// Класс страницы памяти
+class MemoryPage implements Serializable {
+  // Номер страницы
+  private int number;
+
+  // Статус
+  private String status;
+
+  // Размер
+  private int size;
+
+  // Конструктор
+  public MemoryPage(int number, int size) {}
 }
