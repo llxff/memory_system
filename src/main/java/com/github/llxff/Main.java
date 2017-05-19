5ba82552aa4ae13e53e304fcaa72ce17a5ce03ed
Александр Фомин <ll.wg.bin@gmail.com>
2017-05-19 16:25:37 +0300

    fixes

--- src/main/java/com/github/llxff/Main.java
+++ src/main/java/com/github/llxff/Main.java
@@ -6,107 +6,85 @@ public class Main {
   }
 }
 
-// Класс упорядоченного однонаправленного связанного списка, содержащий объекты типа T
-class OrderedLinkedList<T> {
+// Класс упорядоченного однонаправленного связанного списка, содержащий процессы
+class ProcessesList {
   // Класс элемента списка
-  class Item {
+  class ProcessItem {
     // Непосредственно объект данных
-    private T subject;
+    private Process process;
     // Следующий элемент списка
-    private Item next;
+    private ProcessItem next;
 
-    public Item(T subject) {}
+    public ProcessItem(Process process) {}
 
-    // получение объекта данных
-    public T getSubject() {}
-    // получение следующего элемента
-    public Item getNext() {}
-    // метод сравнения двух элементов для упорядочивания списка
-    public int compareTo(Item subject) {}
+    // Получение объекта данных
+    public Process getProcess() {}
+    // Получение следующего элемента
+    public ProcessItem getNext() {}
+    // Установить следующий элемент
+    public void setNext(ProcessItem next) {}
+    // Метод сравнения двух элементов для упорядочивания списка
+    public int compareTo(Process process) {}
   }
 
   // Первый элемент списка
-  private Item head;
+  private Process headProcess;
   // Количество элементов
   private int length;
 
   // Получить первый элемент списка
-  public Item getHead() {}
+  public ProcessItem getHead() {}
   // Пустой ли список?
   public boolean isEmpty() {}
   // Получить количество элементов в списке
   public int getLength() {}
   // Добавить элемент в список
-  public void add(T item) {}
+  public void add(Process process) {}
   // Удалить элемент из списка
   public void remove(int index) {}
-}
-
-// Класс реализации очереди
-class Queue<T> {
-  // Массив элементов
-  private T[] items;
-
-  // Добавить элемент в очередь
-  public void enqueue(T item) {}
-  // Извлечь элемент из очереди
-  public T dequeue() {}
-  // Пустая ли очередь?
-  public boolean isEmpty() {}
-}
-
-// Класс для управления процессами
-class VirtualMachine {
-  // Название файла, в котором хранится состояние
-  private String fileName;
-
-  // Список процессов
-  private OrderedLinkedList<Process> processes;
-
-  // Загрузка состояния
-  public static VirtualMachine load(String fileName) {}
-
-  // Создание нового процесса
-  public Process addProcess() {}
-
-  // Завершение процесса
-  public void killProcess(int pid) {}
-
-  // Сохранение состояния
-  public void save() {}
-
-  // Получить название файла, в котором хранится состояние
-  public String getFileName() {}
 }
 
 // Класс процесса
 class Process {
   // Идентификатор процесса
   private int id;
-
   // Используемые страниц памяти
-  private Queue<MemoryPage> pages;
+  private MemoryPagesQueue pages;
 
   // Конструктор
   public Process(int id) {}
 
+  // Получить идентификатор процесса
   public int getId() {}
-
   // Добавление новой страницы памяти процессу
-  public void addMemoryPage() {}
-
+  public void addMemoryPage(MemoryPage page) {}
   // Удаление страницы памяти процесса
-  public void removeMemoryPage() {}
+  public void removeMemoryPage(int index) {}
+}
 
-  // Завершить процесс и высвободить память
-  public void die() {}
+// Класс реализации очереди страниц памяти
+class MemoryPagesQueue {
+  // Массив элементов
+  private MemoryPage[] pages;
+
+  // Добавить элемент в очередь
+  public void enqueue(MemoryPage page) {}
+  // Извлечь элемент из очереди
+  public MemoryPage dequeue() {}
+  // Пустая ли очередь?
+  public boolean isEmpty() {}
+  // Получить размер очереди
+  public int getLength() {}
+  // Получить страницу памяти по индексу
+  public MemoryPage get(int index) {}
+  // Удалить страницу памяти по индексу
+  public void remove(int index) {}
 }
 
 // Класс страницы памяти
 class MemoryPage {
   // Номер страницы
   private int number;
-
   // Статус
   private String status;
 
@@ -115,7 +93,21 @@ class MemoryPage {
 
   // Получить номер страницы
   public int getNumber() {}
-
   // Получить статус страницы
   public String getStatus() {}
+}
+
+// Класс для сохранения/загрузки процессов
+class VirtualMachine {
+  // Название файла, в котором хранится состояние
+  private String fileName;
+
+  public VirtualMachine(String fileName) {}
+
+  // Получить название файла, в котором хранится состояние
+  public String getFileName() {}
+  // Сохранение процессов
+  public void save(ProcessesList processes) {}
+  // Загрузка процессов
+  public ProcessesList load() {}
 }
